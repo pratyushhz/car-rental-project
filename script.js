205f24cfb276cd04715e2c7aaa8aa25bb538ebb2
pratyush <pratyushhazarika44@gmail.com>
2025-04-28 01:34:51 +0530

    slider

--- script.js
+++ script.js
@@ -154,6 +154,40 @@
 //         });
 //     });
 
+/*
+** With Slick Slider Plugin : https://github.com/marvinhuebner/slick-animation
+** And Slick Animation Plugin : https://github.com/marvinhuebner/slick-animation
+*/
+
+// Init slick slider + animation
+$(document).ready(function(){
+
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 800,
+    lazyLoad: 'progressive',
+    arrows: true,
+    dots: false,
+    pauseOnHover: false,
+    prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></use></svg></div>',
+    nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></use></svg></div>',
+  }).slickAnimation();
+
+  // Arrow click animate
+  $('.slick-nav').on('click touch', function(e) {
+    e.preventDefault();
+    var arrow = $(this);
+    if(!arrow.hasClass('animate')) {
+      arrow.addClass('animate');
+      setTimeout(() => {
+        arrow.removeClass('animate');
+      }, 1600);
+    }
+  });
+
+});
+
 
 
 function showCategory(category) {
