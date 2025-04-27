0f84fb9e91b4b9a8fe8a341f88dcdae1ebd6347b
pratyush <pratyushhazarika44@gmail.com>
2025-04-28 00:49:20 +0530

    booking form

--- script.js
+++ script.js
@@ -187,4 +187,33 @@ function searchVehicle() {
 }
 
 
+
+$(document).ready(function(){
+    $(".reviews-container").owlCarousel({
+        loop:true,
+        margin:20,
+        nav:true,
+        dots:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        navText: [
+            '<i class="bx bx-chevron-left" style="font-size:30px;"></i>',
+            '<i class="bx bx-chevron-right" style="font-size:30px;"></i>'
+        ],
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+});
+
+
     
